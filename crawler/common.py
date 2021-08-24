f945620b3ed482f053ebc660b19031e58d00be6e
dolphin235 <dolphin222@daum.net>
2021-08-24 16:38:00 +0900

    retry up to 3 times in case of url request error

--- crawler/common.py
+++ crawler/common.py
@@ -1,10 +1,31 @@
 #!/bin/user/python3
 import requests
+import time
+import urllib.request
+
+MAX_RETRY = 3
 
 def get_url(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response
-    else:
-        print(f"[ERROR] Get url fail [{response.status_code}]")
-        raise
+    return request_url(url)
+
+
+def request_url(url):
+    count = 0
+    while True:
+        if count >= MAX_RETRY:
+            raise
+        try:
+            headers = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'}
+            #response = requests.get(url, headers=headers)
+            response = urllib.request.urlopen(url)
+            if response.status == 200:
+                return response
+            else:
+                print(f"[ERROR] Get url fail response: [{response.status_code}] - [{url}]")
+                raise
+        except Exception as err:
+            print(err)
+            print(f"[ERROR] Get url fail - [{url}]")
+            time.sleep(60)
+        count = count + 1
+
